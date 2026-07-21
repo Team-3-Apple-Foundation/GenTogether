cbce6d37e5551e2ba3191237065053b903c329be
Ameya More <thinkameya17@gmail.com>
2026-07-21 10:39:52 +1000

    fixed the flow of working app.

--- GenTogether/Screens/HomeView.swift
+++ GenTogether/Screens/HomeView.swift
@@ -2,10 +2,13 @@
 import SwiftUI
 
 struct HomeView: View {
+    /// Borrowed from RootTabView so Play can switch us to the Journey tab.
+    @Binding var selectedTab: AppTab
+
     var body: some View {
         NavigationStack {
-            NavigationLink {
-                GameView(challenge: Challenge.samples[2])
+            Button {
+                selectedTab = .journey
             } label: {
                 Label("Today's game", systemImage: "play.fill")
                     .font(.title3.weight(.semibold))
@@ -19,5 +22,5 @@ struct HomeView: View {
 }
 
 #Preview {
-    HomeView()
+    HomeView(selectedTab: .constant(.home))
 }
